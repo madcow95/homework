6af70bf296ab73c9f05ca6c598e01ab484bc49e2
madcow95 <lobasketve@naver.com>
2021-02-20 23:29:11 +0900

    Feb20

---
+++ Feb_20/src/Feb20_Repeat/Feb20_String_01.java
@@ -0,0 +1,20 @@
+package Feb20_Repeat;
+
+public class Feb20_String_01 {
+	
+	public static void main(String args[]) {
+		
+		int[] strNum = new int[3];
+		
+		strNum[0] = 20;
+		strNum[1] = 55;
+		strNum[2] = 44;
+		
+		for(int i = 0 ; i < strNum.length ; i++) {
+			
+			System.out.println(strNum[i]);
+		}
+		
+	}
+
+}
